d61f62d949aa9d918aa00077100efc3b4476f422
Toby <toby@local>
2026-07-03 09:44:48 +0100

    v2.2.12 - added maps link

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 
     // Bump this string on every deploy (keep version.json in sync).
 
-    var APP_VERSION = "2.2.11";
+    var APP_VERSION = "2.2.12";
 
     var useFileProtocol = window.location.protocol === "file:";
 
