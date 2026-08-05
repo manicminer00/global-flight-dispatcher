0fe91fdf92262cf741f91735f464ebc5c3df749f
Toby <toby@local>
2026-08-05 21:04:51 +0100

    Bump application version to 2.3.15

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 
     // Bump this string on every deploy (keep version.json in sync).
 
-    var APP_VERSION = "2.3.14";
+    var APP_VERSION = "2.3.15";
 
     var useFileProtocol = window.location.protocol === "file:";
 
