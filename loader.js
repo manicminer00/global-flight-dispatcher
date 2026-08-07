21d53d0d8887dc9312da575598ec701e5003a9b3
Toby <toby@local>
2026-08-07 13:17:10 +0100

    Add third-party airport data and bump version

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 
     // Bump this string on every deploy (keep version.json in sync).
 
-    var APP_VERSION = "2.3.17";
+    var APP_VERSION = "2.3.18";
 
     var useFileProtocol = window.location.protocol === "file:";
 
